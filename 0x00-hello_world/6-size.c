29b1488f5c0bc84da8e98f914bc4f7d7db32c6ba
edfmirkel <ah.guettib@gmail.com>
2022-02-03 15:50:10 +0100

    Write a C program that prints the size of various types on the computer it is compiled and run on.
    You should produce the exact same output as in the example
    Warnings are allowed
    Your program should return 0
    You might have to install the package libc6-dev-i386 on your Linux (Vagrant) to test the -m32 gcc option
    julien@ubuntu:~/c/0x00$ gcc 6-size.c -m32 -o size32 2> /tmp/32
    julien@ubuntu:~/c/0x00$ gcc 6-size.c -m64 -o size64 2> /tmp/64
    julien@ubuntu:~/c/0x00$ ./size32
    Size of a char: 1 byte(s)
    Size of an int: 4 byte(s)
    Size of a long int: 4 byte(s)
    Size of a long long int: 8 byte(s)
    Size of a float: 4 byte(s)
    julien@ubuntu:~/c/0x00$ ./size64
    Size of a char: 1 byte(s)
    Size of an int: 4 byte(s)
    Size of a long int: 8 byte(s)
    Size of a long long int: 8 byte(s)
    Size of a float: 4 byte(s)
    julien@ubuntu:~/c/0x00$ echo 0
    0
    julien@ubuntu:~/c/0x00$

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -15,10 +15,10 @@ float floatType;
 /*
 * sizeof evaluates the size of a variable
 */
-printf("Size of a char:: %zu bytes\n", sizeof(charType));
+printf("Size of a char: %zu bytes\n", sizeof(charType));
 printf("Size of an int: %zu bytes\n", sizeof(intType));
 printf("Size of a long int: %zu bytes\n", sizeof(lintType));
 printf("Size of a long long int: %zu byte\n", sizeof(llintType));
-printf("Size of a float:  %zu byte\n", sizeof(floatType));
+printf("Size of a float: %zu byte\n", sizeof(floatType));
 return (0);
 }
