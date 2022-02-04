d5343296f1709d7057cf7f07e19d0ba5d173e5d4
edfmirkel <ah.guettib@gmail.com>
2022-02-04 23:14:47 +0100

    Write a program that prints all possible combinations of single-digit numbers.
    Numbers must be separated by ,, followed by a space
    Numbers should be printed in ascending order
    You can only use the putchar function (every other function (printf, puts, etcâ¦) is forbidden)
    All your code should be in the main function
    You can only use putchar four times maximum in your code
    You are not allowed to use any variable of type char
    julien@ubuntu:~/0x01$ gcc -Wall -pedantic -Werror -Wextra -std=gnu89 9-print_comb.c -o 9-print_comb
    julien@ubuntu:~/0x01$ ./9-print_comb | cat -e
    0, 1, 2, 3, 4, 5, 6, 7, 8, 9$
    julien@ubuntu:~/0x01$

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+#include <math.h>
+/* more headers goes there */
+
+/**
+*  main -  prints all possible combinations of single-digit numbers
+*
+* Return: numbers separate by goma
+*/
+int main(void)
+{
+int n = 48;
+while (n >= 48 && n <= 57)
+{
+putchar(n);
+if (n != 57){
+putchar(',');
+putchar(' ');
+}
+n++;
+}
+putchar('$');
+return (0);
+}
