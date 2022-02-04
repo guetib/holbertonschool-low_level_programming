2e3a1371a010741be0180ae040a4a0677102df93
edfmirkel <ah.guettib@gmail.com>
2022-02-04 15:12:53 +0100

    This program will assign a random number to the variable n each time it is executed. Complete the source code in order to print whether the number stored in the variable n is positive or negative.
    You can find the source code here
    The variable n will store a different value every time you will run this program
    You donât have to understand what rand, srand, RAND_MAX do. Please do not touch this code
    The output of the program should be:
    The number, followed by
    if the number is greater than 0: is positive
    if the number is 0: is zero
    if the number is less than 0: is negative
    followed by a new line
    julien@ubuntu:~/0x01$ gcc -Wall -pedantic -Werror -Wextra -std=gnu89 0-positive_or_negative.c -o 0-positive_or_negative
    julien@ubuntu:~/0x01$ ./0-positive_or_negative
    -520693284 is negative
    julien@ubuntu:~/0x01$ ./0-positive_or_negative
    -973398895 is negative
    julien@ubuntu:~/0x01$ ./0-positive_or_negative
    -199220452 is negative
    julien@ubuntu:~/0x01$ ./0-positive_or_negative
    561319348 is positive
    julien@ubuntu:~/0x01$ ./0-positive_or_negative
    561319348 is positive
    julien@ubuntu:~/0x01$ ./0-positive_or_negative
    266853958 is positive
    julien@ubuntu:~/0x01$ ./0-positive_or_negative
    -48147767 is negative
    julien@ubuntu:~/0x01$ ./0-positive_or_negative
    0 is zero
    julien@ubuntu:~/0x01$

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -17,9 +17,9 @@ n = rand() - RAND_MAX / 2;
 /* your code goes there */
 if (n > 0)
 printf(" %d is positive\n", n);
-if (n < 0)
+else if (n < 0)
 printf("%d is negative\n", n);
-if (n == 0)
+else (n == 0)
 printf("%d is zero\n", n);
 return (0);
 }
