480be5c9c84a501b4d36823157e67a0b3eabb4c4
edfmirkel <ah.guettib@gmail.com>
2022-02-04 16:31:48 +0100

    This program will assign a random number to the variable n each time it is executed. Complete the source code in order to print the last digit of the number stored in the variable n.
    You can find the source code here
    The variable n will store a different value every time you run this program
    You donât have to understand what rand, srand, and RAND_MAX do. Please do not touch this code
    The output of the program should be:
    The string Last digit of, followed by
    n, followed by
    the string is, followed by
    if the last digit of n is greater than 5: the string and is greater than 5
    if the last digit of n is 0: the string and is 0
    if the last digit of n is less than 6 and not 0: the string and is less than 6 and not 0
    followed by a new line
    julien@ubuntu:~/0x01$ gcc -Wall -pedantic -Werror -Wextra -std=gnu89 1-last_digit.c -o 1-last_digit
    julien@ubuntu:~/0x01$ ./1-last_digit
    Last digit of 629438752 is 2 and is less than 6 and not 0
    julien@ubuntu:~/0x01$ ./1-last_digit
    Last digit of -748255693 is -3 and is less than 6 and not 0
    julien@ubuntu:~/0x01$ ./1-last_digit
    Last digit of -1052791662 is -2 and is less than 6 and not 0
    julien@ubuntu:~/0x01$ ./1-last_digit
    Last digit of -284805734 is -4 and is less than 6 and not 0
    julien@ubuntu:~/0x01$ ./1-last_digit
    Last digit of -284805734 is -4 and is less than 6 and not 0
    julien@ubuntu:~/0x01$ ./1-last_digit
    Last digit of 491506926 is 6 and is greater than 5
    julien@ubuntu:~/0x01$ ./1-last_digit
    Last digit of 954249937 is 7 and is greater than 5
    julien@ubuntu:~/0x01$ ./1-last_digit
    Last digit of 652334952 is 2 and is less than 6 and not 0
    julien@ubuntu:~/0x01$ ./1-last_digit
    Last digit of -729688197 is -7 and is less than 6 and not 0
    julien@ubuntu:~/0x01$ ./1-last_digit
    Last digit of -729688197 is -7 and is less than 6 and not 0
    julien@ubuntu:~/0x01$ ./1-last_digit
    Last digit of 45528266 is 6 and is greater than 5
    julien@ubuntu:~/0x01$ ./1-last_digit
    Last digit of 45528266 is 6 and is greater than 5
    julien@ubuntu:~/0x01$ ./1-last_digit
    Last digit of 809065140 is 0 and is 0
    julien@ubuntu:~/0x01$

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -24,6 +24,6 @@ printf("Last digit of %d is %d and is greater than 5\n", n, der);
 else if (der == 0)
 printf("Last digit of %d is %d and is 0\n", n, der);
 else if (der < 6 && n != 0)
-printf("Last digit of %d is %d is less than 6 and not 0\n", n);
+printf("Last digit of %d is negative and is less than 6 and not 0\n", n);
 return (0);
 }
