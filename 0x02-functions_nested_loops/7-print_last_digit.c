66f9d6ebb9127a88cd66b30ce461ebb47b757c92
edfmirkel <ah.guettib@gmail.com>
2022-02-09 22:41:46 +0100

    Write a function that prints the last digit of a number.
    Prototype: int print_last_digit(int);
    Returns the value of the last digit
    julien@ubuntu:~/0x02$ cat 7-main.c
    #include 'main.h'
    
    /**
     * main - check the code
     *
     * Return: Always 0.
     */
    int main(void)
    {
        int r;
    
        print_last_digit(98);
        print_last_digit(0);
        r = print_last_digit(-1024);
        _putchar('0' + r);
        _putchar('\n');
        return (0);
    }
    julien@ubuntu:~/0x02$ gcc -Wall -pedantic -Werror -Wextra -std=gnu89 _putchar.c 7-main.c 7-print_last_digit.c -o 7-last_digit
    julien@ubuntu:~/0x02$ ./7-last_digit
    8044
    julien@ubuntu:~/0x02$

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ *print_last_digit - function that prints the last digit of a number.
+ *@n: integer
+ *Return: @n the value of the last digit
+ */
+int print_last_digit(int n)
+{
+	int v = abs(n % 10) + '0';
+	 _putchar (v);
+	 return (v - '0');
+}
