b01081a40cff0253b0ede78cb5c48da3f82c7e52
edfmirkel <ah.guettib@gmail.com>
2022-02-09 22:06:30 +0100

    Write a function that computes the absolute value of an integer.
    Prototype: int _abs(int);
    FYI: The standard library provides a similar function: abs. Run man abs to learn more.
    
    julien@ubuntu:~/0x02$ cat 6-main.c
    #include 'main.h'
    #include <stdio.h>
    
    /**
     * main - check the code
     *
     * Return: Always 0.
     */
    int main(void)
    {
        int r;
    
        r = _abs(-1);
        printf('%d\n', r);
        r = _abs(0);
        printf('%d\n', r);
        r = _abs(1);
        printf('%d\n', r);
        r = _abs(-98);
        printf('%d\n', r);
        return (0);
    }
    julien@ubuntu:~/0x02$ gcc -Wall -pedantic -Werror -Wextra -std=gnu89 _putchar.c 6-main.c 6-abs.c -o 6-abs
    julien@ubuntu:~/0x02$ ./6-abs
    1
    0
    1
    98
    julien@ubuntu:~/0x02$

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ *_abs - function that computes the absolute value of an integer.
+ *@n: int
+ *Return: @n value
+ */
+
+int _abs(int n)
+{
+	/* if (n > 0) */
+	/* { */
+	return (abs(n));
+	/* } */
+	/* else if (n == 0) */
+	/* { */
+}
