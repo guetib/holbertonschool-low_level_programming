8794d68536cbdd069994fd20d385f7924646cfc4
edfmirkel <ah.guettib@gmail.com>
2022-02-10 14:15:57 +0100

    Score: 0.00% (Checks completed: 0.00%) Write a function that prints all natural numbers from n to 98, followed by a new line.
    Prototype: void print_to_98(int n);
    Numbers must be separated by a comma, followed by a space
    Numbers should be printed in order
    The first printed number should be the number passed to your function
    The last printed number should be 98
    You are allowed to use the standard library
    julien@ubuntu:~/0x02$ cat 11-main.c
    #include 'main.h'
    
    /**
     * main - check the code
     *
     * Return: Always 0.
     */
    int main(void)
    {
        print_to_98(0);
        print_to_98(98);
        print_to_98(111);
        print_to_98(81);
        print_to_98(-10);
        return (0);
    }
    julien@ubuntu:~/0x02$ gcc -Wall -pedantic -Werror -Wextra -std=gnu89 _putchar.c 11-main.c 11-print_to_98.c -o 11-98
    julien@ubuntu:~/0x02$ ./11-98
    0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98
    98
    111, 110, 109, 108, 107, 106, 105, 104, 103, 102, 101, 100, 99, 98
    81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98
    -10, -9, -8, -7, -6, -5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98
    julien@ubuntu:~/0x02$

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,23 +1,22 @@
 #include  <stdio.h>
 #include "main.h"
-
 /**
  *print_to_98 - function that prints all natural numbers from n to 98, followed
  *by a new line
- *
+ *@n: integer
  *Return: Nothing
  */
 void print_to_98(int n)
 {
-	if ( 98 < n)
+	if (n > 98)
 	{
-		for (; n >= 97; n--)
+		for (;n  > 98; --n)
 			printf("%d, ", n);
 	}
 	else
 	{
-		for (; n <  97; n++)
-			 printf("%d, ", n);
+		for (; n <= 97; n++)
+			printf("%d, ", n);
 	}
-	printf ("98\n");
+	printf("98\n");
 }
