258e8ca90a9fbbd6a9fced13fddbd290b17b6f36
edfmirkel <ah.guettib@gmail.com>
2022-02-10 17:29:56 +0100

    first commit

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -17,11 +17,11 @@ int largest_number(int a, int b, int c)
 	b = abs(b);
 	c = abs(c);
 
-	if (a > b && b > c)
+	if (a >= b && b >= c)
 	{
 		largest = a;
 	}
-	else if (b > a && a > c)
+	else if (b >= a && a >= c)
 	{
 		largest = b;
 	}
