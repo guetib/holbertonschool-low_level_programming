0db4d36c6e002ac9df3ed3f29ed9cb4538274eb1
edfmirkel <ah.guettib@gmail.com>
2022-02-10 10:28:26 +0100

    Write a function that prints the 9 times table, starting with 0.
    Prototype: void times_table(void);
    Format: see example
    julien@ubuntu:~/0x02$ cat 9-main.c
    #include 'main.h'
    
    /**
     * main - check the code
     *
     * Return: Always 0.
     */
    int main(void)
    {
        times_table();
        return (0);
    }
    julien@ubuntu:~/0x02$ gcc -Wall -pedantic -Werror -Wextra -std=gnu89 _putchar.c 9-main.c 9-times_table.c -o 9-times_table
    ulien@ubuntu:~/0x02$ ./9-times_table | cat -e
    0,  0,  0,  0,  0,  0,  0,  0,  0,  0$
    0,  1,  2,  3,  4,  5,  6,  7,  8,  9$
    0,  2,  4,  6,  8, 10, 12, 14, 16, 18$
    0,  3,  6,  9, 12, 15, 18, 21, 24, 27$
    0,  4,  8, 12, 16, 20, 24, 28, 32, 36$
    0,  5, 10, 15, 20, 25, 30, 35, 40, 45$
    0,  6, 12, 18, 24, 30, 36, 42, 48, 54$
    0,  7, 14, 21, 28, 35, 42, 49, 56, 63$
    0,  8, 16, 24, 32, 40, 48, 56, 64, 72$
    0,  9, 18, 27, 36, 45, 54, 63, 72, 81$
    julien@ubuntu:~/0x02$ ./9-times_table | tr ' ' . | cat -e
    0,..0,..0,..0,..0,..0,..0,..0,..0,..0$
    0,..1,..2,..3,..4,..5,..6,..7,..8,..9$
    0,..2,..4,..6,..8,.10,.12,.14,.16,.18$
    0,..3,..6,..9,.12,.15,.18,.21,.24,.27$
    0,..4,..8,.12,.16,.20,.24,.28,.32,.36$
    0,..5,.10,.15,.20,.25,.30,.35,.40,.45$
    0,..6,.12,.18,.24,.30,.36,.42,.48,.54$
    0,..7,.14,.21,.28,.35,.42,.49,.56,.63$
    0,..8,.16,.24,.32,.40,.48,.56,.64,.72$
    0,..9,.18,.27,.36,.45,.54,.63,.72,.81$
    julien@ubuntu:~/0x02$

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ *times_table - unction that prints the 9 times table, starting with 0.
+ *
+ *Return: Nothing
+ */
+void times_table(void)
+{
+	int i = 0;
+
+	for (; i <= 9; i++)
+	{
+		int j = 0;
+		
+		while ( j <=9 )
+		{
+			int v = i*j;
+			if ( v > 9 )
+			{
+			_putchar (v / 10 + '0');
+			_putchar (v % 10 + '0');
+			}
+			else
+				_putchar (i*j + '0');
+			if (j != 9)
+			{
+				/*printf ("\n table = %d \n", table[i]);*/
+				_putchar (',');
+				_putchar (' ');
+			}else
+				_putchar ('$');
+			j++;
+		}
+		_putchar ('\n');
+	}
+}
