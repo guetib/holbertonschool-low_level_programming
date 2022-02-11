7c492ac1705214c8cf12fe3ff82fe5b2b62c7679
edfmirkel <ah.guettib@gmail.com>
2022-02-12 00:44:47 +0100

    Write a function that prints a square, followed by a new line.
    Prototype: void print_square(int size);
    You can only use _putchar function to print
    Where size is the size of the square
    If size is 0 or less, the function should print only a new line
    Use the character # to print the square
    julien@ubuntu:~/0x04$ cat 8-main.c
    #include 'main.h'
    
    /**
     * main - check the code
     *
     * Return: Always 0.
     */
    int main(void)
    {
        print_square(2);
        print_square(10);
        print_square(0);
        return (0);
    }
    
    julien@ubuntu:~/0x04$ gcc -Wall -pedantic -Werror -Wextra -std=gnu89 _putchar.c 8-main.c 8-print_square.c -o 8-squares
    julien@ubuntu:~/0x04$ ./8-squares
    ##
    ##
    ##########
    ##########
    ##########
    ##########
    ##########
    ##########
    ##########
    ##########
    ##########
    ##########
    
    julien@ubuntu:~/0x04$

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ *print_line - function that draws a straight line in the terminal.
+ * followed by a new line.
+ *@n:int
+ *Return: void
+ */
+
+void print_line(int n)
+{
+	int i = 1;
+
+	for (; i < n++; i++)
+	{
+		if( i
+		_putchar(' ');
+	}
+	_putchar('\n');
+}
---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ *print_square - function that prints a square, followed by a new line.
+ *@size: integer
+ *Return: void
+ */
+
+void print_square(int size)
+{
+	int i = 0, j = 0;
+
+	for (; i < size; i++)
+	{
+		for (; j < size; j++)
+		{
+			_putchar('#');
+		}
+		_putchar('\n');
+		j = 0;
+	}
+	if (size == 0)
+		_putchar('\n');
+}
