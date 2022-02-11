c8d5f9e331484ae2e44a6e058e2ccb9e2839f48b
edfmirkel <ah.guettib@gmail.com>
2022-02-12 00:15:33 +0100

    Write a function that prints 10 times the numbers, from 0 to 14, followed by a new line.
    Prototype: void more_numbers(void);
    You can only use _putchar three times in your code
    julien@ubuntu:~/0x04$ cat 5-main.c
    #include 'main.h'
    
    /**
     * main - check the code
     *
     * Return: Always 0.
     */
    int main(void)
    {
        more_numbers();
        return (0);
    }
    julien@ubuntu:~/0x04$ gcc -Wall -pedantic -Werror -Wextra -std=gnu89 _putchar.c 5-main.c 5-more_numbers.c -o 5-more_numbers
    julien@ubuntu:~/0x04$ ./5-more_numbers
    01234567891011121314
    01234567891011121314
    01234567891011121314
    01234567891011121314
    01234567891011121314
    01234567891011121314
    01234567891011121314
    01234567891011121314
    01234567891011121314
    01234567891011121314
    julien@ubuntu:~/0x04

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ *more_numbers - function that prints 10 times the numbers, from 0 to 14,
+ * followed by a new line.
+ *
+ *Return: void
+ */
+
+void more_numbers(void)
+{
+	int i = 0, j = '0';
+
+	for (; i < 10; i++)
+	{
+		for (; j <= 14; j++)
+		{
+			if (j > 9)
+				_putchar(j / 10 + '0');
+			_putchar(j % 10 + '0');
+		}
+		_putchar('\n');
+		j = 0;
+	}
+}
