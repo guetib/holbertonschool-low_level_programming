b65db9280b763888c2c4738721c4210edb8f45cd
edfmirkel <ah.guettib@gmail.com>
2022-02-11 15:35:55 +0100

    This program converts a date to the day of year and determines how many days are left in the year, taking leap year into consideration.
    carrie@ubuntu:/debugging$ cat 3-main_a.c
    #include <stdio.h>
    #include 'main.h'
    
    /**
    * main - takes a date and prints how many days are left in the year, taking
    * leap years into account
    * Return: 0
    */
    
    int main(void)
    {
        int month;
        int day;
        int year;
    
        month = 4;
        day = 01;
        year = 1997;
    
        printf('Date: %02d/%02d/%04d\n', month, day, year);
    
        day = convert_day(month, day);
    
        print_remaining_days(month, day, year);
    
        return (0);
    }
    
    carrie@ubuntu:/debugging$
    carrie@ubuntu:/debugging$ cat 3-convert_day.c
    #include 'main.h'
    
    /**
    * convert_day - converts day of month to day of year, without accounting
    * for leap year
    * @month: month in number format
    * @day: day of month
    * Return: day of year
    */
    
    int convert_day(int month, int day)
    {
        switch (month)
        {
            case 2:
                day = 31 + day;
                break;
            case 3:
                day = 59 + day;
                break;
            case 4:
                day = 90 + day;
                break;
            case 5:
                day = 120 + day;
                break;
            case 6:
                day = 151 + day;
                break;
            case 7:
                day = 181 + day;
                break;
            case 8:
                day = 212 + day;
                break;
            case 9:
                day = 243 + day;
                break;
            case 10:
                day = 273 + day;
                break;
            case 11:
                day = 304 + day;
                break;
            case 12:
                day = 334 + day;
                break;
            default:
                break;
        }
        return (day);
    }
    
    carrie@ubuntu:/debugging$
    carrie@ubuntu:/debugging$ cat 3-print_remaining_days.c
    #include <stdio.h>
    #include 'main.h'
    
    /**
    * print_remaining_days - takes a date and prints how many days are
    * left in the year, taking leap years into account
    * @month: month in number format
    * @day: day of month
    * @year: year
    * Return: void
    */
    
    void print_remaining_days(int month, int day, int year)
    {
        if ((year % 4 == 0 || year % 400 == 0) && !(year % 100 == 0))
        {
            if (month >= 2 && day >= 60)
            {
                day++;
            }
    
            printf('Day of the year: %d\n', day);
            printf('Remaining days: %d\n', 366 - day);
        }
        else
        {
            if (month == 2 && day == 60)
            {
                printf('Invalid date: %02d/%02d/%04d\n', month, day - 31, year);
            }
            else
            {
                printf('Day of the year: %d\n', day);
                printf('Remaining days: %d\n', 365 - day);
            }
        }
    }
    
    carrie@ubuntu:/debugging$
    carrie@ubuntu:/debugging$ gcc -Wall -Werror -Wextra -pedantic -std=gnu89 3-convert_day.c 3-print_remaining_days.c 3-main_a.c -o 3-main_a
    carrie@ubuntu:/debugging$ ./3-main_a
    Date: 04/01/1997
    Day of the year: 91
    Remaining days: 274
    carrie@ubuntu:/debugging$
    Output looks good for 04/01/1997! Letâs make a new main file 3-main_b.c to try a case that is a leap year: 02/29/2000.

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -14,10 +14,12 @@ void print_remaining_days(int month, int day, int year)
 {
 	if ((year % 4 == 0/*|| year % 400 == 0) && !(year % 100 == 0*/))
 	{
-		 if (month >= 2 && day >= 60)
-		 {
-		 	day++;
-		 }
+		/**
+		 * if (month >= 2 && day >= 60)
+		 *{
+		 *	day++;
+		 *	}
+		 */
 		printf("Day of the year: %d\n", day);
 		printf("Remaining days:366 - %d\n", 366 - day);
 	}
