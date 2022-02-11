4ee8b394a5bc231332d9afb1a14f9299f7f353cb
edfmirkel <ah.guettib@gmail.com>
2022-02-11 23:26:24 +0100

    Write a function that prints the numbers, from 0 to 9, followed by a new line.
    Prototype: void print_numbers(void);
    You can only use _putchar twice in your code
    julien@ubuntu:~/0x04$ cat 3-main.c
    #include 'main.h'
    
    /**
     * main - check the code
     *
     * Return: Always 0.
     */
    int main(void)
    {
        print_numbers();
        return (0);
    }
    julien@ubuntu:~/0x04$ gcc -Wall -pedantic -Werror -Wextra -std=gnu89 _putchar.c 3-main.c 3-print_numbers.c -o 3-print_numbers
    julien@ubuntu:~/0x04$ ./3-print_numbers | cat -e
    0123456789$
    julien@ubuntu:~/0x04$

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ *print_numbers - function that multiplies two integers.
+ *
+ *Return: void
+ */
+
+void print_numbers(void)
+{
+	int i = 0;
+	for(; i < 10; i++)
+		_putchar(i + '0');
+	_putchar('\n');
+}
