117db5658c6b7791bd9ac176e47cc72928860abe
edfmirkel <ah.guettib@gmail.com>
2022-02-11 23:35:14 +0100

    Write a function that prints the numbers, from 0 to 9, followed by a new line.
    Prototype: void print_most_numbers(void);
    Do not print 2 and 4
    You can only use _putchar twice in your code
    julien@ubuntu:~/0x04$ cat 4-main.c
    #include 'main.h'
    
    /**
     * main - check the code
     *
     * Return: Always 0.
     */
    int main(void)
    {
        print_most_numbers();
        return (0);
    }
    julien@ubuntu:~/0x04$ gcc -Wall -pedantic -Werror -Wextra -std=gnu89 _putchar.c 4-main.c 4-print_most_numbers.c -o 4-print_most_numbers
    julien@ubuntu:~/0x04$ ./4-print_most_numbers
    01356789
    julien@ubuntu:~/0x04$

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ *print_most_numbers - function that prints the numbers, from 0 to 9,
+ * followed by a new line.
+ *
+ *Return: void
+ */
+
+void print_most_numbers(void)
+{
+	int i = 0;
+
+	for (; i < 10; i++)
+	{
+		if (i != 2 && i != 4)
+			_putchar(i + '0');
+	}
+			_putchar('\n');
+}
