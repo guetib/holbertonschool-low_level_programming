e3988c17ed0a033a5d75f77f79b9aafc47cd401b
edfmirkel <ah.guettib@gmail.com>
2022-02-14 13:46:08 +0100

    Write a function that takes a pointer to an int as parameter and updates the value it points to to 98.
    Prototype: void reset_to_98(int *n);
    julien@ubuntu:~/0x05$ cat 0-main.c
    #include 'main.h'
    #include <stdio.h>
    
    /**
     * main - check the code
     *
     * Return: Always 0.
     */
    int main(void)
    {
        int n;
    
        n = 402;
        printf('n=%d\n', n);
        reset_to_98(&n);
        printf('n=%d\n', n);
        return (0);
    }
    julien@ubuntu:~/0x05$ gcc -Wall -pedantic -Werror -Wextra -std=gnu89 0-main.c 0-reset_to_98.c -o 0-98
    julien@ubuntu:~/0x05$ ./0-98
    n=402
    n=98
    julien@ubuntu:~/0x05$

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+/**
+ *reset_to_98 - function that takes a pointer to an int as parameter and
+ *updates the value it points to to 98.
+ *@n: Pointer
+ *Return : Void
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
