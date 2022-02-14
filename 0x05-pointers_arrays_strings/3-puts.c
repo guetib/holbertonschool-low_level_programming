f96f9b0504bc0b200ce5d34ffefbeb1610f7cd9e
edfmirkel <ah.guettib@gmail.com>
2022-02-14 19:20:39 +0100

    Write a function that prints a string, followed by a new line, to stdout.
    Prototype: void _puts(char *str);
    FYI: The standard library provides a similar function: puts. Run man puts to learn more.
    
    julien@ubuntu:~/0x05$ cat 3-main.c
    #include 'main.h'
    
    /**
     * main - check the code
     *
     * Return: Always 0.
     */
    int main(void)
    {
        char *str;
    
        str = 'I do not fear computers. I fear the lack of them - Isaac Asimov';
        _puts(str);
        return (0);
    }
    julien@ubuntu:~/0x05$ gcc -Wall -pedantic -Werror -Wextra -std=gnu89 _putchar.c 3-main.c 3-puts.c -o 3-puts
    julien@ubuntu:~/0x05$ ./3-puts
    I do not fear computers. I fear the lack of them - Isaac Asimov
    julien@ubuntu:~/0x05$

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -4,3 +4,13 @@
  *_puts - function that prints a string, followed by a new line, to stdout.
  *@str: pointer Dereferencing at string.
  *Return : @str
+ */
+
+void _puts(char *str)
+{
+	while (*str != '\0')
+	{
+		_putchar(*str++);
+	}
+	_putchar('\n');
+}
