383ede23d216dd4271f56080c8dc76e9fea09823
edfmirkel <ah.guettib@gmail.com>
2022-02-14 14:00:27 +0100

    Write a function that returns the length of a string.
    Prototype: int _strlen(char *s);
    FYI: The standard library provides a similar function: strlen. Run man strlen to learn more.
    
    julien@ubuntu:~/0x05$ cat 2-main.c
    #include 'main.h'
    #include <stdio.h>
    
    /**
     * main - check the code
     *
     * Return: Always 0.
     */
    int main(void)
    {
        char *str;
        int len;
    
        str = 'My first strlen!';
        len = _strlen(str);
        printf('%d\n', len);
        return (0);
    }
    julien@ubuntu:~/0x05$ gcc -Wall -pedantic -Werror -Wextra -std=gnu89 2-main.c 2-strlen.c -o 2-strlen
    julien@ubuntu:~/0x05$ ./2-strlen
    16
    julien@ubuntu:~/0x05$

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,11 @@
+#include <string.h>
+
+/**
+ *_strlen - function that returns the length of a string.
+ *@s: Pointer
+ *Return: the length of a string.
+ */
+int _strlen(char *s)
+{
+	return (strlen(s));
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,1 +1,2 @@
 void reset_to_98(int *n);
+int _strlen(char *s);
