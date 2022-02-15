5179de4218947cf0fd951496d9d18c76b3b50b32
edfmirkel <ah.guettib@gmail.com>
2022-02-15 15:39:07 +0100

    Write a function that prints every other character of a string, starting with the first character, followed by a new line.
    Prototype: void puts2(char *str);
    julien@ubuntu:~/0x05$ cat 6-main.c
    #include 'main.h'
    
    /**
     * main - check the code
     *
     * Return: Always 0.
     */
    int main(void)
    {
        char *str;
    
        str = '0123456789';
        puts2(str);
        return (0);
    }
    julien@ubuntu:~/0x05$ gcc -Wall -pedantic -Werror -Wextra -std=gnu89 _putchar.c 6-main.c 6-puts2.c -o 6-puts2
    julien@ubuntu:~/0x05$ ./6-puts2
    02468
    julien@ubuntu:~/0x05$

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,5 +1,6 @@
 #include "main.h"
 #include <stdlib.h>
+/* #include <string.h> */
 /**
  *puts2 - function that prints every other character of a string,
  * starting with the first character, followed by a new line.
@@ -9,15 +10,17 @@
 
 void puts2(char *str)
 {
-	while (*str != '\0' || *str == '\0')
+
+	while (/*i <= strlen(*str)*/*str != '\0')
 	{
 		_putchar(*str++);
-		str++;
-		if(*str == '\0')
+		if (*str == '\0')
 		{
-			_putchar('\n');
-			break;
+		_putchar('\n');
+		break;
 		}
+		str++;
+		/* i++; */
 	}
-	/* _putchar('\n'); */
+	_putchar('\n');
 }
