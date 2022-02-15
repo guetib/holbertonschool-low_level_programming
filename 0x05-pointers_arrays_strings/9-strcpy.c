73c12efb48c03055e5a01bdc420caeaea6b85df1
edfmirkel <ah.guettib@gmail.com>
2022-02-15 21:05:55 +0100

    Prototype: char *_strcpy(char *dest, char *src); Write a function that copies the string pointed to by src, including the terminating null byte (\0), to the buffer pointed to by dest.
    Return value: the pointer to dest
    FYI: The standard library provides a similar function: strcpy. Run man strcpy to learn more.
    
    julien@ubuntu:~/0x05$ cat 9-main.c
    #include 'main.h'
    #include <stdio.h>
    
    /**
     * main - check the code
     *
     * Return: Always 0.
     */
    int main(void)
    {
        char s1[98];
        char *ptr;
    
        ptr = _strcpy(s1, 'First, solve the problem. Then, write the code\n');
        printf('%s', s1);
        printf('%s', ptr);
        return (0);
    }
    julien@ubuntu:~/0x05$ gcc -Wall -pedantic -Werror -Wextra -std=gnu89 9-main.c 9-strcpy.c -o 9-strcpy
    julien@ubuntu:~/0x05$ ./9-strcpy
    First, solve the problem. Then, write the code
    First, solve the problem. Then, write the code
    julien@ubuntu:~/0x05$

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stddef.h>
+#include <string.h>
+/**
+ *_strcpy - function that copies the string pointed to by src,
+ *including the terminating null byte (\0), to the buffer pointed to by dest.
+ *@dest:pointer of table
+ *@src:pointer
+ *Return: string
+ */
+char *_strcpy(char *dest, char *src)
+{
+	size_t i, n = strlen(src);
+
+	for (i = 0 ; i < n && src[i] != '\0' ; i++)
+		dest[i] = src[i];
+	for ( ; i < n ; i++)
+		dest[i] = '\0';
+	return (dest);
+}
