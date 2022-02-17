2fc05ce13ec80de3a8eba32c4db03d29fa186604
EdfMirkel <ah.guettib@gmail.com>
2022-02-17 03:19:26 -0800

    Write a function that concatenates two strings.
    Prototype: char *_strcat(char *dest, char *src);
    This function appends the src string to the dest string, overwriting the terminating null byte (\0) at the end of dest, and then adds a terminating null byte
    Returns a pointer to the resulting string dest
    FYI: The standard library provides a similar function: strcat. Run man strcat to learn more.
    
    julien@ubuntu:~/0x06$ cat 0-main.c
    #include 'main.h'
    #include <stdio.h>
    
    /**
     * main - check the code
     *
     * Return: Always 0.
     */
    int main(void)
    {
        char s1[98] = 'Hello ';
        char s2[] = 'World!\n';
        char *ptr;
    
        printf('%s\n', s1);
        printf('%s', s2);
        ptr = _strcat(s1, s2);
        printf('%s', s1);
        printf('%s', s2);
        printf('%s', ptr);
        return (0);
    }
    julien@ubuntu:~/0x06$ gcc -Wall -pedantic -Werror -Wextra -std=gnu89 0-main.c 0-strcat.c -o 0-strcat
    julien@ubuntu:~/0x06$ ./0-strcat
    Hello
    World!
    Hello World!
    World!
    Hello World!
    julien@ubuntu:~/0x06$

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <string.h>
+#include <stdio.h>
+
+/**
+ **_strcat - function that concatenates two strings.
+ *@dest: pointer caracter
+ *@src: pointer caracter
+ *Return: @dest of pointer type
+ */
+
+char *_strcat(char *dest, char *src)
+{
+  int len = strlen(dest), i, n = strlen(src);
+  for (i = 0 ; i <= n && *src != '\0'; i++)
+    {
+      printf("%c\n", *dest);
+      dest[len + i] = src[i];
+      /* src++; */
+      /* dest++; */
+    }
+  return (dest);
+}
