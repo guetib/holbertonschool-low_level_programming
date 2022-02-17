cc5706c7855e711f733f8ced22e9a85d8643b67a
EdfMirkel <ah.guettib@gmail.com>
2022-02-17 06:15:20 -0800

    Write a function that compares two strings.
    Prototype: int _strcmp(char *s1, char *s2);
    Your function should work exactly like strcmp
    FYI: The standard library provides a similar function: strcmp. Run man strcmp to learn more.
    
    julien@ubuntu:~/0x06$ cat 3-main.c
    #include 'main.h'
    #include <stdio.h>
    
    /**
     * main - check the code
     *
     * Return: Always 0.
     */
    int main(void)
    {
        char s1[] = 'Hello';
        char s2[] = 'World!';
    
        printf('%d\n', _strcmp(s1, s2));
        printf('%d\n', _strcmp(s2, s1));
        printf('%d\n', _strcmp(s1, s1));
        return (0);
    }
    julien@ubuntu:~/0x06$ gcc -Wall -pedantic -Werror -Wextra -std=gnu89 3-main.c 3-strcmp.c -o 3-strcmp
    julien@ubuntu:~/0x06$ ./3-strcmp
    -15
    15
    0
    julien@ubuntu:~/0x06$

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <string.h>
+/* #include <stdio.h> */
+
+/**
+ *_strcmp - function that compares two strings.
+ *@s1: pointer caracter
+ *@s2: pointer caracter
+ *@n: integer
+ *Return: integer of pointer type
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	int i = strcmp(s1, s2);
+
+	return (i);
+}
