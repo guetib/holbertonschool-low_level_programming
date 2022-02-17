82a867c5bb0822c79ef7efc8e1bef37003e92db0
EdfMirkel <ah.guettib@gmail.com>
2022-02-17 05:54:08 -0800

    Write a function that copies a string.
    Prototype: char *_strncpy(char *dest, char *src, int n);
    Your function should work exactly like strncpy
    FYI: The standard library provides a similar function: strncpy. Run man strncpy to learn more.
    
    julien@ubuntu:~/0x06$ cat 2-main.c
    #include 'main.h'
    #include <stdio.h>
    
    /**
     * main - check the code
     *
     * Return: Always 0.
     */
    int main(void)
    {
        char s1[98];
        char *ptr;
        int i;
    
        for (i = 0; i < 98 - 1; i++)
        {
            s1[i] = '*';
        }
        s1[i] = '\0';
        printf('%s\n', s1);
        ptr = _strncpy(s1, 'First, solve the problem. Then, write the code\n', 5);
        printf('%s\n', s1);
        printf('%s\n', ptr);
        ptr = _strncpy(s1, 'First, solve the problem. Then, write the code\n', 90);
        printf('%s', s1);
        printf('%s', ptr);
        for (i = 0; i < 98; i++)
        {
            if (i % 10)
            {
                printf(' ');
            }
            {
                printf('\n');
            }
            printf('0x%02x', s1[i]);
        }
        printf('\n');
        return (0);
    }
    julien@ubuntu:~/0x06$ gcc -Wall -pedantic -Werror -Wextra -std=gnu89 2-main.c 2-strncpy.c -o 2-strncpy
    julien@ubuntu:~/0x06$ ./2-strncpy
    *************************************************************************************************
    First********************************************************************************************
    First********************************************************************************************
    First, solve the problem. Then, write the code
    First, solve the problem. Then, write the code
    0x46 0x69 0x72 0x73 0x74 0x2c 0x20 0x73 0x6f 0x6c
    0x76 0x65 0x20 0x74 0x68 0x65 0x20 0x70 0x72 0x6f
    0x62 0x6c 0x65 0x6d 0x2e 0x20 0x54 0x68 0x65 0x6e
    0x2c 0x20 0x77 0x72 0x69 0x74 0x65 0x20 0x74 0x68
    0x65 0x20 0x63 0x6f 0x64 0x65 0x0a 0x00 0x00 0x00
    0x00 0x00 0x00 0x00 0x00 0x00 0x00 0x00 0x00 0x00
    0x00 0x00 0x00 0x00 0x00 0x00 0x00 0x00 0x00 0x00
    0x00 0x00 0x00 0x00 0x00 0x00 0x00 0x00 0x00 0x00
    0x00 0x00 0x00 0x00 0x00 0x00 0x00 0x00 0x00 0x00
    0x2a 0x2a 0x2a 0x2a 0x2a 0x2a 0x2a 0x00
    julien@ubuntu:~/0x06$

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -12,12 +12,12 @@
 
 char *_strncpy(char *dest, char *src, int n)
 {
-  /* size_t dest_len = strlen(dest); */
-  int i;
-  
-  for (i = 0; i < n && src[i] != '\0'; i++)
-    dest[i] = src[i];
-  for ( ; i < n; i++)
-    dest[i] = '\0';
-  return (dest);
+	/* size_t dest_len = strlen(dest); */
+	int i;
+
+	for (i = 0; i < n && src[i] != '\0'; i++)
+		dest[i] = src[i];
+	for ( ; i < n; i++)
+		dest[i] = '\0';
+	return (dest);
 }
