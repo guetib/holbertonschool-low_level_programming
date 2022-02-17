5243162dca9f66033134b18f746bdc1aafaa5da4
EdfMirkel <ah.guettib@gmail.com>
2022-02-17 04:25:12 -0800

    Write a function that concatenates two strings.
    Prototype: char *_strncat(char *dest, char *src, int n);
    The _strncat function is similar to the _strcat function, except that
    it will use at most n bytes from src; and
    src does not need to be null-terminated if it contains n or more bytes
    Return a pointer to the resulting string dest
    FYI: The standard library provides a similar function: strncat. Run man strncat to learn more.
    
    julien@ubuntu:~/0x06$ cat 1-main.c
    #include 'main.h'
    #include <stdio.h>
    
    /**
     * main - check the code
     *
     * Return: Always 0.
     */
    int main(void)
    {
        char s1[98] = 'Hello ';
        char s2[] = 'World!\n';
        char *ptr;
    
        printf('%s\n', s1);
        printf('%s', s2);
        ptr = _strncat(s1, s2, 1);
        printf('%s\n', s1);
        printf('%s', s2);
        printf('%s\n', ptr);
        ptr = _strncat(s1, s2, 1024);
        printf('%s', s1);
        printf('%s', s2);
        printf('%s', ptr);
        return (0);
    }
    julien@ubuntu:~/0x06$ gcc -Wall -pedantic -Werror -Wextra -std=gnu89 1-main.c 1-strncat.c -o 1-strncat
    julien@ubuntu:~/0x06$ ./1-strncat
    Hello
    World!
    Hello W
    World!
    Hello W
    Hello WWorld!
    World!
    Hello WWorld!
    julien@ubuntu:~/0x06$

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <string.h>
+/* #include <stdio.h> */
+
+/**
+ **_strncat - function that concatenates two strings.
+ *@dest: pointer caracter
+ *@src: pointer caracter
+ *Return: @dest of pointer type
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+int len = strlen(dest), i;
+
+for (i = 0 ; i < n /*&& *src != '\0' */; i++)
+{
+ /* printf("%d\n", i); */
+dest[len + i] = src[i];
+/* src++; */
+/* dest++; */
+}
+/* printf("i = %d", i); */
+/* dest[len + i] = '\0'; */
+return (dest);
+}
