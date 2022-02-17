e9f5464da9a5e9da8ebd7145f07993eab52a18e2
EdfMirkel <ah.guettib@gmail.com>
2022-02-17 08:37:42 -0800

    Write a function that capitalizes all words of a string.
    Prototype: char *cap_string(char *);
    julien@ubuntu:~/0x06$ cat 6-main.c
    #include 'main.h'
    #include <stdio.h>
    
    /**
     * main - check the code
     *
     * Return: Always 0.
     */
    int main(void)
    {
        char str[] = 'Expect the best. Prepare for the worst. Capitalize on what comes.\nhello world! hello-world 0123456hello world\thello world.hello world\n';
        char *ptr;
    
        ptr = cap_string(str);
        printf('%s', ptr);
        printf('%s', str);
        return (0);
    }
    julien@ubuntu:~/0x06$ gcc -Wall -pedantic -Werror -Wextra -std=gnu89 6-main.c 6-cap_string.c -o 6-cap
    julien@ubuntu:~/0x06$ ./6-cap
    Expect The Best. Prepare For The Worst. Capitalize On What Comes.
    Hello World! Hello-world 0123456hello World Hello World.Hello World
    Expect The Best. Prepare For The Worst. Capitalize On What Comes.
    Hello World! Hello-world 0123456hello World Hello World.Hello World
    julien@ubuntu:~/0x06$

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /* #include <string.h> */
-/* #include <stdio.h> */
+#include <stdio.h>
 
 /**
  **cap_string - function that compares two strings.
@@ -13,8 +13,17 @@ char *cap_string(char *s)
 	int i = 0;
 
 	for (; s[i] != '\0'; i++)
-		if (s[i] >= 97 && s[i] <= 122)
-			s[i] = s[i] - 32;
+	{
+		if (s[i] == ' ')
+		{
+			i++;
+			if (s[i] >= 97 && s[i] <= 122)
+				s[i] = s[i] - 32;
+			else if (s[i] == '\0')
+				i--;
+		}
+		/* printf("%c", s[i]); */
+	}
 
 	return (s);
 }
