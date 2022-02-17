0e336b221b977344228c3ff7b159d1c0bd593c33
EdfMirkel <ah.guettib@gmail.com>
2022-02-17 04:16:02 -0800

    Write a function that concatenates two strings.
    Prototype: char *_strcat(char *dest, char *src);
    This function appends the src string to the dest string, overwriting the terminating null byte (\0) at the end of dest, and then adds a terminating null byte
    Returns a pointer to the resulting string dest
    FYI: The standard library provides a similar function: strcat. Run man strcat to learn more.
    
    julien@ubuntu:~/0x06$ cat 0-main.c
    #include 'main.h'
    #include <stdio.h>
    
    /**
     * main - check the code
     *
     * Return: Always 0.
     */
    int main(void)
    {
        char s1[98] = 'Hello ';
        char s2[] = 'World!\n';
        char *ptr;
    
        printf('%s\n', s1);
        printf('%s', s2);
        ptr = _strcat(s1, s2);
        printf('%s', s1);
        printf('%s', s2);
        printf('%s', ptr);
        return (0);
    }
    julien@ubuntu:~/0x06$ gcc -Wall -pedantic -Werror -Wextra -std=gnu89 0-main.c 0-strcat.c -o 0-strcat
    julien@ubuntu:~/0x06$ ./0-strcat
    Hello
    World!
    Hello World!
    World!
    Hello World!
    julien@ubuntu:~/0x06$

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,6 +1,6 @@
 #include "main.h"
 #include <string.h>
-#include <stdio.h>
+/* #include <stdio.h> */
 
 /**
  **_strcat - function that concatenates two strings.
@@ -11,15 +11,16 @@
 
 char *_strcat(char *dest, char *src)
 {
-  int len = strlen(dest), i, n = strlen(src);
-  for (i = 0 ; i <= n /*&& *src != '\0' */; i++)
-    {
-      /* printf("%d\n", i); */
-      dest[len + i] = src[i];
-      /* src++; */
-      /* dest++; */
-    }
-  /* printf("i = %d", i); */
-  /* dest[len + i] = '\0'; */
-  return (dest);
+int len = strlen(dest), i, n = strlen(src);
+
+for (i = 0 ; i <= n /*&& *src != '\0' */; i++)
+{
+ /* printf("%d\n", i); */
+dest[len + i] = src[i];
+/* src++; */
+/* dest++; */
+}
+/* printf("i = %d", i); */
+/* dest[len + i] = '\0'; */
+return (dest);
 }
