b4df2548bf1a21e2a0affc98149eaccbdad35682
edfmirkel <ah.guettib@gmail.com>
2022-02-19 02:14:06 +0100

    Write a function that encodes a string into 1337.
    Letters a and A should be replaced by 4
    Letters e and E should be replaced by 3
    Letters o and O should be replaced by 0
    Letters t and T should be replaced by 7
    Letters l and L should be replaced by 1
    Prototype: char *leet(char *);
    You can only use one if in your code
    You can only use two loops in your code
    You are not allowed to use switch
    You are not allowed to use any ternary operation
    julien@ubuntu:~/0x06$ cat 7-main.c
    #include 'main.h'
    #include <stdio.h>
    
    /**
     * main - check the code for
     *
     * Return: Always 0.
     */
    int main(void)
    {
        char s[] = 'Expect the best. Prepare for the worst. Capitalize on what comes.\n';
        char *p;
    
        p = leet(s);
        printf('%s', p);
        printf('%s', s);
        return (0);
    }
    julien@ubuntu:~/0x06$ gcc -Wall -pedantic -Werror -Wextra -std=gnu89 7-main.c 7-leet.c -o 7-1337
    julien@ubuntu:~/0x06$ ./7-1337
    3xp3c7 7h3 b3s7. Pr3p4r3 f0r 7h3 w0rs7. C4pi741iz3 0n wh47 c0m3s.
    3xp3c7 7h3 b3s7. Pr3p4r3 f0r 7h3 w0rs7. C4pi741iz3 0n wh47 c0m3s.
    julien@ubuntu:~/0x06$

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,41 @@
+#include "main.h"
+/* #include <string.h> */
+#include <stdio.h>
+
+/**
+ **leet - unction that encodes a string into 1337.
+ *Letters a and A should be replaced by 4
+ *Letters e and E should be replaced by 3
+ *Letters o and O should be replaced by 0
+ *Letters t and T should be replaced by 7
+ *Letters l and L should be replaced by 1
+ *@s: pointer caracter
+ *Return: string of pointer type
+ */
+
+char *leet(char *s)
+{
+	int i = 0, j;
+	char sc[] = {'a', 'e', 'o', 't', 'l',};
+	char n[] = {'4', '3', '0', '7', '1',};
+
+	while (s[i])
+	{
+		/* printf ("s[i] = %c, ", s[i]); */
+		j = 0;
+
+		while (j < 5)
+		{
+			if (s[i] == sc[j] || s[i] - 32 == sc[j])
+			{
+				s[i] = n[j];
+				break;
+			}
+
+			j++;
+		}
+
+		i++;
+	}
+	return (s);
+}
