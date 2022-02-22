bc17c19c454718256069d122876989f835fb288b
edfmirkel <ah.guettib@gmail.com>
2022-02-22 13:35:43 +0100

    Write a function that locates a substring.
    Prototype: char *_strstr(char *haystack, char *needle);
    The _strstr() function finds the first occurrence of the substring needle in the string haystack. The terminating null bytes (\0) are not compared
    Returns a pointer to the beginning of the located substring, or NULL if the substring is not found.
    FYI: The standard library provides a similar function: strstr. Run man strstr to learn more.
    
    julien@ubuntu:~/0x07$ cat 5-main.c
    #include 'main.h'
    #include <stdio.h>
    
    /**
     * main - check the code
     *
     * Return: Always 0.
     */
    int main(void)
    {
        char *s = 'hello, world';
        char *f = 'world';
        char *t;
    
        t = _strstr(s, f);
        printf('%s\n', t);
        return (0);
    }
    julien@ubuntu:~/0x07$ gcc -Wall -pedantic -Werror -Wextra -std=gnu89 5-main.c 5-strstr.c -o 5-strstr
    julien@ubuntu:~/0x07$ ./5-strstr
    world
    julien@ubuntu:~/0x07$

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,35 @@
+#include "main.h"
+#include <string.h>
+/* #include <stdio.h> */
+/**
+ **_strstr -function that locates a substring.
+ *@haystack: poiter of char type
+ *@needle: pointer of caracter type
+ *Return: Returns a pointer to the beginning of the located substring, or NULL
+ * if the substring is not found.
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+	char *p = NULL;
+	int i = 0;
+
+	for (;/* i < (int)strlen(haystack)*/haystack[i]/* != '\0' */ ; i++)
+	{
+		int j = 0;
+
+		for (; j < (int)strlen(needle); j++)
+		{
+			/* printf("str = %d", i); */
+			if (needle[j] == haystack[i])
+			{
+				p = &haystack[i];
+				/* printf ("p = %c", *p); */
+			}
+			else
+					break;
+		}
+	}
+
+	return (p);
+}
