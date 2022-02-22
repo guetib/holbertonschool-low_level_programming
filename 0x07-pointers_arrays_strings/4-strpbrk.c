61e222bfb1a6a46b6e9adcfa515be110afe2b589
edfmirkel <ah.guettib@gmail.com>
2022-02-22 10:10:20 +0100

    Write a function that searches a string for any of a set of bytes.
    Prototype: char *_strpbrk(char *s, char *accept);
    The _strpbrk() function locates the first occurrence in the string s of any of the bytes in the string accept
    Returns a pointer to the byte in s that matches one of the bytes in accept, or NULL if no such byte is found
    FYI: The standard library provides a similar function: strpbrk. Run man strpbrk to learn more.
    
    julien@ubuntu:~/0x07$ cat 4-main.c
    #include 'main.h'
    #include <stdio.h>
    
    /**
     * main - check the code
     *
     * Return: Always 0.
     */
    int main(void)
    {
        char *s = 'hello, world';
        char *f = 'world';
        char *t;
    
        t = _strpbrk(s, f);
        printf('%s\n', t);
        return (0);
    }
    julien@ubuntu:~/0x07$ gcc -Wall -pedantic -Werror -Wextra -std=gnu89 4-main.c 4-strpbrk.c -o 4-strpbrk
    julien@ubuntu:~/0x07$ ./4-strpbrk
    llo, world
    julien@ubuntu:~/0x07$

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -10,7 +10,7 @@
  * that matches one of the bytes in accept, or NULL if no such byte is found
  */
 
-char *_strpbrk(char *s, char *accept);
+char *_strpbrk(char *s, char *accept)
 {
 	char *p = strpbrk(s, accept);
 
