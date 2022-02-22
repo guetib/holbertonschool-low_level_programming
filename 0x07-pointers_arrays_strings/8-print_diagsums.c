8a4c49321f0c103b0e9155c47814ede19ecbdb2c
edfmirkel <ah.guettib@gmail.com>
2022-02-23 00:19:30 +0100

    Write a function that prints the sum of the two diagonals of a square matrix of integers.
    Prototype: void print_diagsums(int *a, int size);
    Format: see example
    You are allowed to use the standard library
    Note that in the following example we are casting an int[][] into an int*. This is not something you should do. The goal here is to make sure you understand how an array of array is stored in memory.
    
    julien@ubuntu:~/0x07$ cat 8-main.c
    #include 'main.h'
    #include <stdio.h>
    
    /**
     * main - check the code
     *
     * Return: Always 0.
     */
    int main(void)
    {
        int c3[3][3] = {
            {0, 1, 5},
            {10, 11, 12},
            {1000, 101, 102},
        };
        int c5[5][5] = {
            {0, 1, 5, 12124, 1234},
            {10, 11, 12, 123521, 12512},
            {1000, 101, 102, 12545, 214543435},
            {100, 1012451, 11102, 12545, 214543435},
            {10, 12401, 10452, 11542545, 1214543435},
        };
        print_diagsums((int *)c3, 3);
        print_diagsums((int *)c5, 5);
        return (0);
    }
    julien@ubuntu:~/0x07$ gcc -Wall -pedantic -Werror -Wextra -std=gnu89 8-main.c 8-print_diagsums.c -o 8-print_diagsums
    julien@ubuntu:~/0x07$ ./8-print_diagsums
    113, 1016
    1214556093, 1137318
    julien@ubuntu:~/0x07$

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_chessboard - function that prints the sum of the two diagonals
+ * of a square matrix of integers.
+ * @a: ponter of matrix
+ *@size: integer
+ * Return: Void.
+ */
+
+void print_diagsums(int *a, int size)
+{
+	int i = 0, j = size - 1, sum = 0;
+
+	for (; i < size * size; i += size +1)
+	{
+		sum += a[i];
+	}
+	printf ("%d, ", sum);
+
+	sum = 0;
+	for (; j < (size * size) - size + 1; j += size - 1)
+	{
+		sum += a[j];
+	}
+	printf ("%d\n", sum);
+}
