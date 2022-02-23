2baf3f15f32730ab5ff5c39a7cf5db8e38ffced1
edfmirkel <ah.guettib@gmail.com>
2022-02-23 19:23:50 +0100

    Write a function that returns the factorial of a given number.
    Prototype: int factorial(int n);
    If n is lower than 0, the function should return -1 to indicate an error
    Factorial of 0 is 1
    julien@ubuntu:~/0x08. Recursion$ cat 3-main.c
    #include 'main.h'
    #include <stdio.h>
    
    /**
     * main - check the code
     *
     * Return: Always 0.
     */
    int main(void)
    {
        int r;
    
        r = factorial(1);
        printf('%d\n', r);
        r = factorial(5);
        printf('%d\n', r);
        r = factorial(10);
        printf('%d\n', r);
        r = factorial(-1024);
        printf('%d\n', r);
        return (0);
    }
    julien@ubuntu:~/0x08. Recursion$ gcc -Wall -pedantic -Werror -Wextra -std=gnu89 3-main.c 3-factorial.c -o 3-factorial
    julien@ubuntu:~/0x08. Recursion$ ./3-factorial
    1
    120
    3628800
    -1
    julien@ubuntu:~/0x08. Recursion$

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * factorial - function that returns the factorial of a given number.
+ *@a: integer.
+ * Return: integer of factorial.
+ */
+
+int factorial(int n)
+{
+	int i = 1;
+
+	if (i <= n)
+	{
+		int j = n * factorial(n - 1);
+		return (j);/* (j abs(j));*/
+	}
+	else if ( n == 0)
+		return (1);
+	else
+		return (-1);
+}
