d7be42c2d6a5353842976688462abc438d05fb3c
edfmirkel <ah.guettib@gmail.com>
2022-02-23 20:23:43 +0100

    Write a function that returns the value of x raised to the power of y.
    Prototype: int _pow_recursion(int x, int y);
    If y is lower than 0, the function should return -1
    FYI: The standard library provides a different function: pow. Run man pow to learn more.
    
    julien@ubuntu:~/0x08. Recursion$ cat 4-main.c
    #include 'main.h'
    #include <stdio.h>
    
    /**
     * main - check the code
     *
     * Return: Always 0.
     */
    int main(void)
    {
        int r;
    
        r = _pow_recursion(1, 10);
        printf('%d\n', r);
        r = _pow_recursion(1024, 0);
        printf('%d\n', r);
        r = _pow_recursion(2, 16);
        printf('%d\n', r);
        r = _pow_recursion(5, 2);
        printf('%d\n', r);
        r = _pow_recursion(5, -2);
        printf('%d\n', r);
        r = _pow_recursion(-5, 3);
        printf('%d\n', r);
        return (0);
    }
    julien@ubuntu:~/0x08. Recursion$ gcc -Wall -pedantic -Werror -Wextra -std=gnu89 4-main.c 4-pow_recursion.c -o 4-pow
    julien@ubuntu:~/0x08. Recursion$ ./4-pow
    1
    1
    65536
    25
    -1
    -125
    julien@ubuntu:~/0x08. Recursion$

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -13,16 +13,15 @@
 
 int _pow_recursion(int x, int y)
 {
-
-	if (y < 0)
-		return (-1);
-	else
+	if (y > 0)
 	{
-			if (y == 1)
-				return (x);
-			if (y == 0)
-				return (1);
-			/* printf ("x = %d,  y = %d, ", x, y); */
+		if (y == 1)
+			return (x);
+		if (y == 0)
+			return (1);
+		/* printf ("x = %d,  y = %d, ", x, y); */
 		return (x * _pow_recursion(x, y - 1));
 	}
+	else
+		return (-1);
 }
